6804b0454e336ac0abfc5c0d71044b6be15cbc5b
Patrick Ryan <pjryan@pugetsound.edu>
2018-04-02 00:17:51 -0700

    cleaned up code

--- Quine.java
+++ Quine.java
@@ -3,17 +3,13 @@ import java.io.File;
 
 public class Quine
 {
-
 	public static void main(String[] args)
 	{
-
 		try
 		{		
 			String path = System.getProperty("user.dir") + "\\Quine.java";
 			File f = new File(path);
-
 			Scanner input = new Scanner(f);
-
 			while(input.hasNextLine())
 			{
 				System.out.println(input.nextLine());
